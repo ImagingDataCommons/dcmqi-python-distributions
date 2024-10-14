246bb072b860d3badc097c65504077593df575dc
Leonard Nürnberg <lenny@moontec.de>
2024-10-14 14:40:21 +0200

    feat: update dcmqi from version v1.3.2 to v1.3.4

--- dcmqiUrls.cmake
+++ dcmqiUrls.cmake
@@ -1,15 +1,15 @@
 # Checksum copied from "dcmqi_checksums.txt" associated with the dcmqi GitHub release
 
-set(version "1.3.2")
+set(version "1.3.4")
 
 set(linux_filename    "dcmqi-${version}-linux.tar.gz")
-set(linux_sha256      "bdeb99a05465e568629a7e9f72903e3f3c108ab756856f4f5e1c1a714df7c609")
+set(linux_sha256      "4b9ecc27bde8befd35c0b95449744c3a3b43466592a7211e8f5adbfe9e497608")
 
 set(macos_filename    "dcmqi-${version}-mac.tar.gz")
-set(macos_sha256      "07f785318c3b73af73fed5919080481ef1580dbfdbde2a1aec272736848cc417")
+set(macos_sha256      "691cc64815016b76665c798713dee38447ec8acf9a6f059db5e78415a902c4b0")
 
 set(win64_filename    "dcmqi-${version}-win64.zip")
-set(win64_sha256      "e5a5328743f8128517f21b0ea1e20fd68b2844908f7349a5b40d7b0aa9af7374")
+set(win64_sha256      "f9d5ec1bf9e5b0ba4e2cddabfa8d773afee167a48f71a48ec61549a6dcba06bd")
 
 
 cmake_host_system_information(RESULT is_64bit QUERY IS_64BIT)
